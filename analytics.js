80f7a766a7d5c08cd3807c7ed35b524c8dfbf0a3
Ben Parsons <ben@bpulse.co.uk>
2017-02-14 18:10:03 +0000

    Add Google Analytics

--- analytics.js
+++ analytics.js
@@ -1,3 +1,5 @@
+/* FACEBOOK */
+
 window.fbAsyncInit = function() {
   FB.init({
     appId      : '1924147151151810',
@@ -14,3 +16,13 @@ window.fbAsyncInit = function() {
    js.src = "//connect.facebook.net/en_US/sdk.js";
    fjs.parentNode.insertBefore(js, fjs);
  }(document, 'script', 'facebook-jssdk'));
+
+/* GOOGLE */
+
+(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+})(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+ga('create', 'UA-91932834-1', 'auto');
+ga('send', 'pageview');
