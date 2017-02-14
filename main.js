20af46131e02203ab9ea7980e6cef904d1738e42
Ben Parsons <ben@bpulse.co.uk>
2017-02-14 16:30:49 +0000

    show results from DB after both user votes complete

--- main.js
+++ main.js
@@ -4,6 +4,9 @@ MODE.HATE = 2;
 MODE.RESULTS = 3;
 var current_mode = MODE.LIKE;
 
+var most_liked = {};
+var most_hated = {};
+
 var image_prefix = "/images/";
 if (window.location.href.indexOf('localhost') === -1) {
   image_prefix = "http://www.bpulse.co.uk/emoji/images/";
@@ -24,6 +27,11 @@ function shuffle(array) {
 
 function initialiseGlyphs() {
   $("#container").empty();
+  $("#db_selections").hide();
+
+  most_liked = {};
+  most_hated = {};
+
   $.get("/get-emoji", function(data) {
     $("#emoji_name").text(data[0].emoji_name);
     data = shuffle(data);
@@ -40,7 +48,37 @@ function initialiseGlyphs() {
       box.click(function(event) {
         glyphClick(event, emoji);
       });
+
+      if (!most_liked.like_count ||
+        emoji.like_count && (most_liked.like_count < emoji.like_count) ) {
+        most_liked = JSON.parse(JSON.stringify(emoji));
+      }
+      if (!most_hated.hate_count ||
+        emoji.hate_count && (most_hated.hate_count < emoji.hate_count) ) {
+        most_hated = JSON.parse(JSON.stringify(emoji));
+      }
     }
+
+    $("#db_like").append($("<h2>").text(most_liked.vendor_name));
+    var most_liked_div = $("<div class='glyph'>");
+    var most_liked_img = $("<img>");
+    most_liked_img.attr("width", "72");
+    most_liked_img.attr("src",  image_prefix + most_liked.unicode_value + "_" + most_liked.vendor_name + ".png");
+    most_liked_div.append(most_liked_img);
+    $("#db_like").append(most_liked_div);
+
+    $("#db_hate").append($("<h2>").text(most_hated.vendor_name));
+    var most_hated_div = $("<div class='glyph'>");
+    var most_hated_img = $("<img>");
+    most_hated_img.attr("width", "72");
+    most_hated_img.attr("src",  image_prefix + most_hated.unicode_value + "_" + most_hated.vendor_name + ".png");
+    most_hated_div.append(most_hated_img);
+    $("#db_hate").append(most_hated_div);
+
+    console.log("most liked");
+    console.log(most_liked);
+    console.log("most hated");
+    console.log(most_hated);
   });
 }
 
@@ -58,6 +96,7 @@ function setModeHate() {
 function setModeResults() {
   current_mode = MODE.RESULTS;
   $("#instruction").text("Results:");
+  $("#db_selections").show();
 }
 
 function glyphClick(event, emoji) {
@@ -104,6 +143,7 @@ function fillUserLikeEmoji(emoji, vendor_name) {
   img.attr("width", "72");
   img.attr("src",  image_prefix + emoji.unicode_value + "_" + vendor_name + ".png");
   box.append(img);
+  $("#user_like").append($("<h2>").text(vendor_name));
   $("#user_like").append(box);
 }
 
@@ -114,5 +154,6 @@ function fillUserHateEmoji(emoji, vendor_name) {
   img.attr("width", "72");
   img.attr("src",  image_prefix + emoji.unicode_value + "_" + vendor_name + ".png");
   box.append(img);
+  $("#user_hate").append($("<h2>").text(vendor_name));
   $("#user_hate").append(box);
 }
