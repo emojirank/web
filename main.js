e7b4182b6591e3fc5f47ad6076e69864aa667e2d
Ben Parsons <ben@bpulse.co.uk>
2017-02-17 14:07:04 +0000

    add user vote to current total before showing world rankings

--- main.js
+++ main.js
@@ -7,6 +7,8 @@ var current_mode = MODE.LIKE;
 var most_liked = {};
 var most_hated = {};
 
+var emoji_array = [];
+
 var image_prefix = "/images/";
 if (window.location.href.indexOf('localhost') === -1) {
   image_prefix = "http://www.bpulse.co.uk/emoji/images/";
@@ -32,10 +34,11 @@ function initialiseGlyphs() {
   most_liked = {};
   most_hated = {};
 
-    $.get("/get-emoji/" + window.location.pathname.split('/')[2], function(data) {
+  $.get("/get-emoji/" + window.location.pathname.split('/')[2], function(data) {
     $("#emoji_name").html(data[0].emoji_name);
     document.title = "EmojiRank: " + data[0].emoji_name;
     data = shuffle(data);
+    emoji_array = data;
     for (i in data) {
       var emoji = data[i];
       //console.log(emoji);
@@ -49,38 +52,40 @@ function initialiseGlyphs() {
       box.click(function(event) {
         glyphClick(event, emoji);
       });
-
-      if (!most_liked.like_count ||
-        emoji.like_count && (most_liked.like_count < emoji.like_count) ) {
-        most_liked = JSON.parse(JSON.stringify(emoji));
-      }
-      if (!most_hated.hate_count ||
-        emoji.hate_count && (most_hated.hate_count < emoji.hate_count) ) {
-        most_hated = JSON.parse(JSON.stringify(emoji));
-      }
     }
-
-    var most_liked_div = $("<div class='glyph'>");
-    var most_liked_img = $("<img>");
-    most_liked_img.attr("width", "72");
-    most_liked_img.attr("src",  image_prefix + most_liked.unicode_value + "_" + most_liked.vendor_name + ".png");
-    most_liked_div.append(most_liked_img);
-    $("#db_like").append(most_liked_div);
-    $("#db_like").append($("<h4>").text(most_liked.vendor_name));
-
-    var most_hated_div = $("<div class='glyph'>");
-    var most_hated_img = $("<img>");
-    most_hated_img.attr("width", "72");
-    most_hated_img.attr("src",  image_prefix + most_hated.unicode_value + "_" + most_hated.vendor_name + ".png");
-    most_hated_div.append(most_hated_img);
-    $("#db_hate").append(most_hated_div);
-    $("#db_hate").append($("<h4>").text(most_hated.vendor_name));
-
-    console.log("most liked");
-    console.log(most_liked);
-    console.log("most hated");
-    console.log(most_hated);
   });
+}
+
+function populateWorldResults() {
+  for (i in emoji_array) {
+    var emoji = emoji_array[i];
+    if (!most_liked.like_count ||
+      emoji.like_count && (most_liked.like_count < emoji.like_count) ) {
+      most_liked = JSON.parse(JSON.stringify(emoji));
+    }
+    if (!most_hated.hate_count ||
+      emoji.hate_count && (most_hated.hate_count < emoji.hate_count) ) {
+      most_hated = JSON.parse(JSON.stringify(emoji));
+    }
+  }
+
+  var most_liked_div = $("<div class='glyph'>");
+  var most_liked_img = $("<img>");
+  most_liked_img.attr("width", "72");
+  most_liked_img.attr("src",  image_prefix + most_liked.unicode_value + "_" + most_liked.vendor_name + ".png");
+  most_liked_div.append(most_liked_img);
+  $("#db_like").append(most_liked_div);
+  $("#db_like").append($("<h4>").text(most_liked.vendor_name));
+
+  var most_hated_div = $("<div class='glyph'>");
+  var most_hated_img = $("<img>");
+  most_hated_img.attr("width", "72");
+  most_hated_img.attr("src",  image_prefix + most_hated.unicode_value + "_" + most_hated.vendor_name + ".png");
+  most_hated_div.append(most_hated_img);
+  $("#db_hate").append(most_hated_div);
+  $("#db_hate").append($("<h4>").text(most_hated.vendor_name));
+
+  $("#db_selections").show();
 }
 
 function setModeLike() {
@@ -97,7 +102,7 @@ function setModeHate() {
 function setModeResults() {
   current_mode = MODE.RESULTS;
   $("#instruction").text("Results:");
-  $("#db_selections").show();
+  populateWorldResults();
 }
 
 function glyphClick(event, emoji) {
@@ -111,12 +116,24 @@ function glyphClick(event, emoji) {
 
   if (current_mode == MODE.LIKE) {
     vote = 'like';
-    setModeHate();
     fillUserLikeEmoji(emoji, vendor);
+    for (i in emoji_array) {
+      if (emoji_array[i].vendor_name === vendor) {
+        emoji_array[i].like_count ? emoji_array[i].like_count++ : emoji_array[i].like_count = 1;
+        break;
+      }
+    }
+    setModeHate();
   } else if (current_mode == MODE.HATE) {
     vote = 'hate';
-    setModeResults();
     fillUserHateEmoji(emoji, vendor);
+    for (i in emoji_array) {
+      if (emoji_array[i].vendor_name === vendor) {
+        emoji_array[i].hate_count ? emoji_array[i].hate_count++ : emoji_array[i].hate_count = 1;
+        break;
+      }
+    }
+    setModeResults();
   }
 
   console.log(vendor);
